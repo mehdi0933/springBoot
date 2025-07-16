68b201e50f371219ceb8969fdb780ad7ad942566
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2025-07-16 15:32:46 +0200

    Add files via upload

--- demo/src/main/java/com/formationspring/demo/controllers/ControllersMistralAi.java
+++ demo/src/main/java/com/formationspring/demo/controllers/ControllersMistralAi.java
@@ -24,34 +24,32 @@ public class ControllersMistralAi {
     }
 
     @GetMapping("/ai")
-    public ResponseEntity<String> search(@RequestParam String promptMsg,   @RequestParam String apiKey ) throws IOException, InterruptedException {
+    public ResponseEntity<String> search(@RequestParam String url,   @RequestParam String apiKey ) throws IOException, InterruptedException {
 
-        String url = "https://openrouter.ai/api/v1/chat/completions";
        // String apiKey = "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";
 
-        String requestBody = """
+        String promptMsg = """
         {
             "model": "mistralai/mistral-7b-instruct:free",
             "messages": [
-                { "role": "user", "content": "%s" }
+                { "role": "user", "content": "qui a gagne le plus de fois le grand prix de monaco en F1 ?" }
             ]
         }
-        """.formatted(promptMsg);
-        System.out.println(promptMsg);
+        """;
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .header("Content-Type", "application/json")
-                .header("Authorization", apiKey.trim())
-                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .header("Authorization" , apiKey.trim())
+                .POST(HttpRequest.BodyPublishers.ofString(promptMsg))
                 .build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         String apiResponse = response.body();
 
         saveMistralAi.save(promptMsg,apiKey, url);
-
+        System.out.println(" la reponse de AI "+apiResponse);
         return ResponseEntity.ok(apiResponse + "\n" + LocalDateTime.now());
     }
 }

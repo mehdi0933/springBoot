2e8215a591bb247c01be2fafc849c753e98713dc
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2025-07-20 16:47:59 +0200

    Delete demo directory

--- demo/src/main/java/com/formationspring/demo/services/IUserDataAcces.java
+++
@@ -1,9 +0,0 @@
-package com.formationspring.demo.services;
-
-import com.formationspring.demo.entity.UserEntity;
-import java.util.List;
-
-public interface IUserDataAcces {
-    List<UserEntity> getAllUsers();
-    List<UserEntity> saveAllUsers(List<UserEntity> users);
-}

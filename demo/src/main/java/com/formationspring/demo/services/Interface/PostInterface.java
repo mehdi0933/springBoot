764b241b89eef9eb49484cea1f4d4447643cd351
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2025-07-22 02:26:03 +0200

    Delete demo directory

--- demo/src/main/java/com/formationspring/demo/services/Interface/PostInterface.java
+++
@@ -1,9 +0,0 @@
-package com.formationspring.demo.services.Interface;
-import com.formationspring.demo.DTO.PostDto;
-
-public interface PostInterface {
-
-    PostDto.OutputDto findPostById(int id);
-    PostDto.OutputDto createPost(PostDto post);
-
-}
--- demo/src/main/java/com/formationspring/demo/services/Interface/UserDataAccesInterface.java
+++
@@ -1,9 +0,0 @@
-package com.formationspring.demo.services.Interface;
-import  com.formationspring.demo.DTO.UserDataAccesDto;
-import java.util.List;
-
-public interface UserDataAccesInterface {
-
-    List<UserDataAccesDto.OutputDto> getAllUsers();
-    List<UserDataAccesDto.OutputDto> saveAllUsers(List<UserDataAccesDto.InputDto> users);
-}

39f8b243f648d8b4f8c72e2a6f2bf9710f8eca2e
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2025-07-27 20:57:26 +0200

    Delete demo directory

--- demo/src/main/java/com/formationspring/demo/entity/enums/AiModel.java
+++
@@ -1,9 +0,0 @@
-package com.formationspring.demo.entity.enums;
-
-public enum AiModel {
-
-    MISTRAL,    //("mistralai/mistral-7b-instruct:free"),
-    HUNYUAN,    //("tencent/hunyuan-a13b-instruct:free");
-
-
-}
--- demo/src/main/java/com/formationspring/demo/services/Interface/AiReponseInterface.java
+++
@@ -1,7 +0,0 @@
-package com.formationspring.demo.services.Interface;
-
-import com.formationspring.demo.services.AiExecutorFactory;
-
-public interface AiReponseInterface {
-    String Aireponse(AiExecutorFactory aiReponse);
-}

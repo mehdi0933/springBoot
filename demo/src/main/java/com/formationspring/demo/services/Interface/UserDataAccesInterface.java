4905fe4846e23953ef056cfff31680e4524b00bd
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2025-07-31 03:04:11 +0200

    Delete demo directory

--- demo/src/main/java/com/formationspring/demo/services/Interface/UserDataAccesInterface.java
+++
@@ -1,9 +0,0 @@
-package com.formationspring.demo.services.Interface;
-import  com.formationspring.demo.DTO.UserDataAccesDto;
-import java.util.List;
-
-public interface UserDataAccesInterface {
-
-    List<UserDataAccesDto.Output> getAllUsers();
-    List<UserDataAccesDto.Output> saveAllUsers(List<UserDataAccesDto.Input> users);
-}
